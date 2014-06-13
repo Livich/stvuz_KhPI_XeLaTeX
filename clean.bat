1d454777ba3d769c2f672d12e8b7d1ffb751b845
Serg <sergiy.lilikovych@gmail.com>
2014-06-13 18:11:29 +0300

    Clean script for Windows commit

---
+++ clean.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+del /S *.aux
+del /S *.log
+del /S *.blg
+del /S *.out
+del /S *.fls
+del /S *.fdb_latexmk
+del /S *.bbl
+del /S *.synctex.gz
+del /S *.toc
+del /S *.bst
+echo Successful
